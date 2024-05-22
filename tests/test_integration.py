a667f15c5dea907f8fca748e7e747c66d5e4518f
tatev.baghdasaryan <tatev.baghdasaryan@ispras.ru>
2024-05-22 17:08:57 +0400

    Minor path modifications.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-
+from fastapi.templating import Jinja2Templates
 import os
 import sys
 
@@ -10,8 +10,9 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_predict_endpoint_with_audio_file():
-    audio_file_path = "./covers80_test/coversongs/covers32k/Addicted_To_Love/robert_palmer+Riptide+03-Addicted_To_Love.mp3"
+    audio_file_path = "/home/tatev/Documents/Clustering-music-genres/covers80_test/coversongs/covers32k/Addicted_To_Love/robert_palmer+Riptide+03-Addicted_To_Love.mp3"
 
     response = client.post("/predict", files={"audio_file": open(audio_file_path, "rb")})
 
